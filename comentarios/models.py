8df8958c6b318e1f27fab795717a45927910ebd1
Pedro <phdss04@gmail.com>
2022-11-19 21:09:37 -0300

    changes on form spacing

--- comentarios/models.py
+++ comentarios/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 # Create your models here.
 class Comentario(models.Model):
-    nome_comentario = models.CharField(max_length=100, blank=False, verbose_name='Nome')
+    nome_comentario = models.CharField(max_length=100, verbose_name='Nome')
     email_comentario = models.EmailField(blank=False, verbose_name='Email')
     comentario = models.TextField(blank=False)
     post_comentario = models.ForeignKey(Post, on_delete=models.CASCADE, verbose_name='Post')
